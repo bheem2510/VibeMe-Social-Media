195a56c5b78e11363e02281d180c46b595dc5cc8
Bheem Mastkar <111329157+bheem2510@users.noreply.github.com>
2023-02-08 20:11:25 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ function App() {
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   const isAuth = Boolean(useSelector((state) => state.token));
 
+  
   return (
     <div className="app">
       <BrowserRouter>
